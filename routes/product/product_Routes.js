50456eaea770fae27a1e5c79e13cce3ff9d4febb
KickCode13 <antoniomagnotrabalho@gmail.com>
2024-11-23 11:52:34 -0300

    Add /add-product route to product_Routes and minor adjustments

--- routes/product/product_Routes.js
+++ routes/product/product_Routes.js
@@ -1,7 +1,7 @@
-import e from "express";
+import express from "express";
 import ProductController from "../../controllers/productController.js";
 
-const routerProduct = e.Router();
+const routerProduct = express.Router();
 
 routerProduct.get('/products-page', (req, res) =>{
     res.render('product/products-page');
@@ -11,6 +11,9 @@ routerProduct.get('/product-details', (req, res) =>{
     res.render('product/product-details');
 });
 
+routerProduct.get('/add-product', (req, res) =>{
+    res.render('product/product-add');
+});
 routerProduct.post('/add-product', (req, res) =>{
     ProductController.addProduct(req, res);
 });
