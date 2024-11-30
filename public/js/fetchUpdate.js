1d9405084354ba9a977ed93de88aedf88e61b6cb
KickCode13 <antoniomagnotrabalho@gmail.com>
2024-11-30 12:25:32 -0300

    Create fetchUpdate script for handling product update requests

---
+++ public/js/fetchUpdate.js
@@ -0,0 +1,49 @@
+document.querySelectorAll(".btn-edit").forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      const url = this.getAttribute("data-url");
+      console.log(url);
+      const product = {
+        name: document.getElementById('name').value,
+        category: document.getElementById('category').value,
+        sizeOption: document.getElementById('sizeOption').value,
+        colorOption: document.getElementById('colorOption').value,
+        productFeatures: document.getElementById('productFeatures').value,
+        price: document.getElementById('price').value,
+        quantity: document.getElementById('quantity').value,
+        description: document.getElementById('description').value,
+        image_url: document.getElementById('image_url').value,
+        purchased: document.getElementById('purchased').value,
+        purchaseDate: document.getElementById('purchaseDate').value,
+        buyer: {
+          name: document.getElementById('buyerName').value,
+          email: document.getElementById('buyerEmail').value,
+        },
+        productStatus: document.getElementById('productStatus').value,
+        shippingStatus: document.getElementById('shippingStatus').value,
+      };
+      const confirmEdit = confirm("Deseja mesmo atualizar o Produto?");
+      if (confirmEdit) {
+        fetch(url, {
+             method: "PUT",
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(product)
+         })
+          .then((res) => {
+            if (res.ok) {
+              alert("Produto Editado");
+              window.location.href= '/';
+            } else {
+              alert("Erro ao editar o produto");
+            }
+          })
+          .catch((err) => {
+            console.error("Erro ao deletar produto:", err);
+            alert("Erro inesperado. Tente novamente.");
+          });
+      }
+    });
+  });
+  
