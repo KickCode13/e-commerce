5eca59f2113d5343e8375f27d9fd001662a04ea5
KickCode13 <antoniomagnotrabalho@gmail.com>
2024-12-09 13:28:39 -0300

    Create user model

---
+++ models/User_Model.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+const userModel = mongoose.model("User", 
+    new Schema({
+        name:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        cpf:{
+            type:String,
+            required: true
+        },
+        number:{
+            type:String,
+            required: true
+        }
+    })
+)
+
+export default userModel;
